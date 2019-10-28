3f6483cfb9dff2ef9cac93b480262108037434f6
Roger Enand <rogerenand@gmail.com>
2019-10-28 16:55:02 -0700

    fixed wrong vars

--- es6_v1.js
+++ es6_v1.js
@@ -11,10 +11,10 @@ animalListing(animals, true);
 
 
 function animalListing( animalArray, listFoods=true){
-	var outputArray = [];
+	let outputArray = [];
 	animalArray.map((animalArray)=>{
 		let { type, height, weight, name, top3Foods } = animalArray;
-		let sentance = `Animal: ${type} is a ${type} and is ${height} meters at ${weight*2.2} lbs`;
+		let sentance = `Animal: ${name} is a ${type} and is ${height} meters at ${weight*2.2} lbs`;
 		if (listFoods) sentance += ` and it likes to eat ${top3Foods[0]} ,${top3Foods[1]}, and ${top3Foods[2]}`
 		console.log(sentance);
 	});
